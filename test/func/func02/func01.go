fee766330a292b3c23b25143058deb35be83d629
raye <3187870250@qq.com>
2022-12-02 09:06:03 +0800

    update

--- test/func/func02/func01.go
+++ test/func/func02/func01.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
 type kelvin float64
 
 func measureTemperature(samples int, sensor func() kelvin) {
@@ -8,4 +14,10 @@ func measureTemperature(samples int, sensor func() kelvin) {
 		fmt.Printf("%v' K\n", k)
 		time.Sleep(time.Second)
 	}
+}
+func fakeSensor() kelvin {
+	return kelvin(rand.Intn(151) + 150)
+}
+func main() {
+	measureTemperature(3, fakeSensor)
 }
--- test/go.mod
+++ test/go.mod
@@ -1,3 +1,10 @@
 module test
 
 go 1.19
+
+require (
+	github.com/go-sql-driver/mysql v1.6.0
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
+	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
+	golang.org/x/crypto v0.3.0
+)
