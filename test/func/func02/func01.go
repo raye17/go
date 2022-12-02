331baabd3a5eaaee36af690fdbcec22aab7ac098
raye <3187870250@qq.com>
2022-12-02 09:11:45 +0800

    update

--- test/func/func02/func01.go
+++ test/func/func02/func01.go
@@ -11,7 +11,7 @@ type kelvin float64
 func measureTemperature(samples int, sensor func() kelvin) {
 	for i := 0; i < samples; i++ {
 		k := sensor()
-		fmt.Printf("%v' K\n", k)
+		fmt.Printf("%v K\n", k)
 		time.Sleep(time.Second)
 	}
 }
