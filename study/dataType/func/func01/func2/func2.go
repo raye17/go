98b4af2c393e3c01fdea1736e19880d78e2cd0a3
raye <3187870250@qq.com>
2023-01-04 15:30:58 +0800

    update

--- study/dataType/func/func01/func2/func2.go
+++ study/dataType/func/func01/func2/func2.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import "fmt"
 
+func hello() []string {
+	return nil
+}
+func main() {
+	h := hello
+	if h == nil {
+		fmt.Println("nil")
+	} else {
+		fmt.Println("not nil")
+	}
 }
--- study/test.go
+++ study/test.go
@@ -1,9 +1,6 @@
 package main
 
-import (
-	"flag"
-	"fmt"
-)
+import "fmt"
 
 func containsDuplicate(nums []int) bool {
 	container := make(map[int]int)
@@ -16,21 +13,15 @@ func containsDuplicate(nums []int) bool {
 	}
 	return false
 }
-
+func hello(num ...int) {
+	num[0] = 199
+}
 func main() {
-	msg := "raye hello lcx helper"
-	s := "hello,孙肖扬"
-	for k, v := range s {
-		fmt.Printf("k: %v,v: %c\n", k, v)
-	}
-	n := len(msg)
-	fmt.Println(n)
-	var length = int32(n)
-	fmt.Println(length)
-	fmt.Println("***********")
-	flag.Parse()
-	args := flag.Args()
-	fmt.Println(args)
+	i := []int{1, 2, 3}
+	hello(i...)
+	fmt.Println(i)
+	a := 'a'
+	fmt.Println(int(a))
 }
 func rotate(nums []int, k int) {
 	length := len(nums)
