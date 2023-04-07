831bb4e2e52421c72201f4a91e8d2608b5f081c0
raye <3187870250@qq.com>
2023-04-07 09:02:12 +0800

    update

--- study/test.go
+++ study/test.go
@@ -1,4 +1,17 @@
 package main
 
+import (
+	"github.com/labstack/echo/v4"
+	"net/http"
+)
+
 func main() {
+	r := echo.New()
+	r.GET("/test", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, echo.Map{
+			"message": "test",
+			"data":    "data",
+		})
+	})
+	r.Start("127.0.0.1:8899")
 }
